c98b4bc8f3660bce5b3b585a10dfedc04ee0cdde
lucaspchartier <lucaspchartier@gmail.com>
2018-12-11 13:20:28 -0500

    Added callback timer functions for auth/ui.js.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -6,14 +6,18 @@ const signUpSuccess = function (signUpResponse) {
   $('.auth-message').html('Sign up successful!')
   $('.auth-message').addClass('success-message')
   $('.auth-message').removeClass('error-message')
-  // $('.auth-message').empty('success-message')
+  setTimeout(function () {
+    $('.auth-message').empty()
+  }, 1000)
 }
 
 const failure = function (failureResponse) {
   $('.auth-message').html('Error: Something went wrong.')
   $('.auth-message').addClass('error-message')
   $('.auth-message').removeClass('success-message')
-  // $('.auth-message').empty('error-message')
+  setTimeout(function () {
+    $('.auth-message').empty()
+  }, 1000)
 }
 
 const signInSuccess = function (signInResponse) {
@@ -21,31 +25,37 @@ const signInSuccess = function (signInResponse) {
   $('.auth-message').html('Login successful!')
   $('.auth-message').addClass('success-message')
   $('.auth-message').removeClass('error-message')
-  // $('.auth-message').empty('success-message')
   $('#change-password-form').removeClass('hidden')
   $('#sign-out-button').removeClass('hidden')
   $('#sign-up-form').addClass('hidden')
   $('#sign-in-form').addClass('hidden')
   $('.content').removeClass('hidden')
+  setTimeout(function () {
+    $('.auth-message').empty()
+  }, 1000)
 }
 
 const changePasswordSuccess = function (changePasswordResponse) {
   $('.auth-message').html('Password change successful!')
   $('.auth-message').addClass('success-message')
   $('.auth-message').removeClass('error-message')
-  // $('.auth-message').empty('success-message')
+  setTimeout(function () {
+    $('.auth-message').empty()
+  }, 1000)
 }
 
 const signOutSuccess = function (signOutResponse) {
   $('.auth-message').html('Logout successful!')
   $('.auth-message').addClass('success-message')
   $('.auth-message').removeClass('error-message')
-  // $('.auth-message').empty('success-message')
   $('#change-password-form').addClass('hidden')
   $('#sign-out-button').addClass('hidden')
   $('#sign-up-form').removeClass('hidden')
   $('#sign-in-form').removeClass('hidden')
   $('.content').addClass('hidden')
+  setTimeout(function () {
+    $('.auth-message').empty()
+  }, 1000)
 }
 
 module.exports = {

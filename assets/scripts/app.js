3cb10ead834d7f7aeab3001eb4b09ff98fc63ef3
lucaspchartier <lucaspchartier@gmail.com>
2019-05-21 11:29:51 -0400

    Added form toggle for create artist form.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,6 +30,11 @@ $(() => {
     $('.sign-up-form').slideUp(500)
   })
   $('#change-password-btn').click(function () {
+    $('.create-artist-form').slideDown(300)
     $('.change-password-form').slideDown(500)
+  })
+  $('#create-artist-btn').click(function () {
+    $('.change-password-form').slideDown(300)
+    $('.create-artist-form').slideDown(500)
   })
 })
